c91bdb6838feb88d3ff4dd6fd0a382eb763e4254
wemm <92384868+wshadough@users.noreply.github.com>
2021-10-27 20:56:54 +0200

    updated to not have a stroke on the second try

--- governoredit.sh
+++ governoredit.sh
@@ -1,17 +1,30 @@
 #!/bin/bash
 
 function decision {
-read -p "Specify the desired cpu governor: " governor
+echo "Here are the options: "
+echo ---
+echo "1. performance
+2. schedutil
+3. powersave
+4. conservative"
+echo --- 
+read -p "Enter the number of the desired cpu governor: " governor
 if [ $governor = 1 ]
 then sudo cpupower -c all frequency-set -g performance
 echo "The PERFORMANCE governor has been selected." && sleep 1
 elif [ $governor = 2 ]
 then sudo cpupower -c all frequency-set -g schedutil
 echo "The SCHEDUTIL governor has been selected." && sleep 1
+elif [ $governor = 3 ]
+then sudo cpupower -c all frequency-set -g powersave
+echo "The POWERSAVE governor has been selected." && sleep 1
+elif [ $governor = 4 ]
+then sudo cpupower -c all frequency-set -g conservative
+echo "The CONSERVATIVE governor has been selected." && sleep 1
 else echo "pls don't try to fool me :("
 sleep 1 && unset $governor
 decision
-fi; 
+fi;
 } 
 
 function testing {
@@ -20,26 +33,35 @@ if [ $choice = "y" ]
 then echo "ok then" 
      sleep 1 && kill -9 $PPID
 elif [ $choice = "n" ]
-then echo "rip" 
-     sleep 1 && kill -9 $PPID
+then echo "rip, i'll dump you into the selector again then" 
+     sleep 1 && clear
+     redo
 else echo "lmao what"
      sleep 1 && testing
 fi;
 }
 
-echo "Here are the options: "
-echo ---
-echo "1. performance
-2. schedutil"
-echo --- 
-
-decision
-
+function declaration {
 sleep 1
 clear
 echo "All done!" 
 sleep 1
 printf "The current governor is: "
 cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+echo --;
+
+}
+
+function redo {
+decision
+declaration
+testing;
+}
+
+decision
+
+declaration
+
+unset $governor
 
 testing
